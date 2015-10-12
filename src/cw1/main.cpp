0795687fea180d88371e4bc9f2093d8ba5e27335
Sam Serrels <doogly1@gmail.com>
2015-10-12 12:27:38 +0100

    Base LinPack

---
+++ src/cw1/main.cpp
@@ -0,0 +1,13 @@
+#include "sequential.h"
+
+// Defines the data size to work on
+const unsigned int SIZE = 1000;
+// Used to validate the result.  This is related to the data size
+const double CHECK_VALUE = 12.0;
+
+int main(int argc, char **argv)
+{
+  seq::start();
+
+  return 0;
+}
---
+++ src/cw1/sequential.h
@@ -0,0 +1,6 @@
+extern const unsigned int SIZE;
+extern const double CHECK_VALUE;
+
+namespace seq {
+  int start();
+}
